46418939226a6a012dac1811a4741f10f7326368
Walter Rothbard <walter.rothbard@gmail.com>
2021-11-15 20:31:36 -0600

    Fix pricing errors for tools

--- recalc.lua
+++ recalc.lua
@@ -90,6 +90,8 @@ simple_amm.recalc = function(pos)
 		-- minetest.log("action", "item1 " .. item1 .. " " .. count1)
 		-- minetest.log("action", "item2 " .. item2 .. " " .. count2)
 
+		local is_tool = (minetest.registered_tools[item1] or minetest.registered_tools[item2])
+
 		if count1 == count2 then
 			-- minetest.log("action", "equal quantities")
 			simple_amm.set_ask(meta, 2)
@@ -98,21 +100,23 @@ simple_amm.recalc = function(pos)
 			inv:set_stack("give3", 1, ItemStack(item1))
 			inv:set_stack("pay4", 1, ItemStack({name = item1, count = 2}))
 			inv:set_stack("give4", 1, ItemStack(item2))
-			local price2 = calc_cost_to_buy(count1, count2, 2)
-			if price2 > 0 then
-				inv:set_stack("pay2", 1, ItemStack({name = item2, count = price2}))
-				inv:set_stack("give2", 1, ItemStack({name = item1, count = 2}))
-				inv:set_stack("pay5", 1, ItemStack({name = item1, count = price2}))
-				inv:set_stack("give5", 1, ItemStack({name = item2, count = 2}))
+			if not is_tool then
+				local price2 = calc_cost_to_buy(count1, count2, 2)
+				if price2 > 0 then
+					inv:set_stack("pay2", 1, ItemStack({name = item2, count = price2}))
+					inv:set_stack("give2", 1, ItemStack({name = item1, count = 2}))
+					inv:set_stack("pay5", 1, ItemStack({name = item1, count = price2}))
+					inv:set_stack("give5", 1, ItemStack({name = item2, count = 2}))
+				end
+				local price3 = calc_cost_to_buy(count1, count2, 3)
+				if price3 > 0 then
+					inv:set_stack("pay1", 1, ItemStack({name = item2, count = price3}))
+					inv:set_stack("give1", 1, ItemStack({name = item1, count = 3}))
+					inv:set_stack("pay6", 1, ItemStack({name = item1, count = price3}))
+					inv:set_stack("give6", 1, ItemStack({name = item2, count = 3}))
+				end
+				return
 			end
-			local price3 = calc_cost_to_buy(count1, count2, 3)
-			if price3 > 0 then
-				inv:set_stack("pay1", 1, ItemStack({name = item2, count = price3}))
-				inv:set_stack("give1", 1, ItemStack({name = item1, count = 3}))
-				inv:set_stack("pay6", 1, ItemStack({name = item1, count = price3}))
-				inv:set_stack("give6", 1, ItemStack({name = item2, count = 3}))
-			end
-			return
 		end
 
 		-- how much does it cost to buy one item1?
@@ -133,32 +137,34 @@ simple_amm.recalc = function(pos)
 			-- minetest.log("action", "quant_of_item2_for_one_item1 (bid): " .. quant_of_item2_for_one_item1)
 		end
 
-		-- now what about a x2 "bulk buy"?
-		local ask2 = calc_cost_to_buy(count1, count2, 2)
-		if ask2 > 0 then
-			inv:set_stack("pay2", 1, ItemStack({name = item2, count = ask2}))
-			inv:set_stack("give2", 1, ItemStack({name = item1, count = 2}))
-			-- minetest.log("action", "cost of two item1 in item2: " .. ask2)
-		end
-		local bid2 = calc_quant_for_spend(count1, count2, 2)
-		if bid2 > 0 then
-			inv:set_stack("pay5", 1, ItemStack({name = item1, count = 2}))
-			inv:set_stack("give5", 1, ItemStack({name = item2, count = bid2}))
-			-- minetest.log("action", "quant of item2 for two item1: " .. bid2)
-		end
+		if not is_tool then
+			-- now what about a x2 "bulk buy"?
+			local ask2 = calc_cost_to_buy(count1, count2, 2)
+			if ask2 > 0 then
+				inv:set_stack("pay2", 1, ItemStack({name = item2, count = ask2}))
+				inv:set_stack("give2", 1, ItemStack({name = item1, count = 2}))
+				-- minetest.log("action", "cost of two item1 in item2: " .. ask2)
+			end
+			local bid2 = calc_quant_for_spend(count1, count2, 2)
+			if bid2 > 0 then
+				inv:set_stack("pay5", 1, ItemStack({name = item1, count = 2}))
+				inv:set_stack("give5", 1, ItemStack({name = item2, count = bid2}))
+				-- minetest.log("action", "quant of item2 for two item1: " .. bid2)
+			end
 
-		-- what about x3 ?
-		local ask3 = calc_cost_to_buy(count1, count2, 3)
-		if ask3 > 0 then
-			inv:set_stack("pay1", 1, ItemStack({name = item2, count = ask3}))
-			inv:set_stack("give1", 1, ItemStack({name = item1, count = 3}))
-			-- minetest.log("action", "cost of three item1 in item2: " .. ask3)
-		end
-		local bid3 = calc_quant_for_spend(count1, count2, 3)
-		if bid3 > 0 then
-			inv:set_stack("pay6", 1, ItemStack({name = item1, count = 3}))
-			inv:set_stack("give6", 1, ItemStack({name = item2, count = bid3}))
-			-- minetest.log("action", "quant of item2 for three item1: " .. bid3)
+			-- what about x3 ?
+			local ask3 = calc_cost_to_buy(count1, count2, 3)
+			if ask3 > 0 then
+				inv:set_stack("pay1", 1, ItemStack({name = item2, count = ask3}))
+				inv:set_stack("give1", 1, ItemStack({name = item1, count = 3}))
+				-- minetest.log("action", "cost of three item1 in item2: " .. ask3)
+			end
+			local bid3 = calc_quant_for_spend(count1, count2, 3)
+			if bid3 > 0 then
+				inv:set_stack("pay6", 1, ItemStack({name = item1, count = 3}))
+				inv:set_stack("give6", 1, ItemStack({name = item2, count = bid3}))
+				-- minetest.log("action", "quant of item2 for three item1: " .. bid3)
+			end
 		end
 	end
 end
